2205186e54b2520132525fd7bd0e709cf2462b0b
Christina Loiacono <loiaconochristina@gmail.com>
2023-02-15 20:34:29 -0500

    Refactor /students routes as controller and query
    This commit moves /students and /students/:id from app.js into a dedicated students controller. It also extracts the data-fetching functions from the controller to a query layer.

---
+++ controllers/studentsController.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const studentsController = express.Router();
+
+const studentsData = require('../data/studentsData.json');
+const { getAllStudents, getStudentById } = require('../queries/studentsQueries');
+
+studentsController.get('/', (request, response) => {
+    try {
+        const students = getAllStudents();
+        response.status(200).json({ data: students });
+    } catch (err) {
+        response.status(500).json({ error: err.message });
+    }
+  });
+  
+studentsController.get('/:id', (request, response) => {
+    try {
+      const { id } = request.params;
+      const student = getStudentById(id);
+    
+      if (student) {
+        // return 200
+        return response.status(200).json( { data: student });
+      }
+      // return 404
+      response.status(404).json({ error: `Could not find student with id ${id}` });
+    } catch (err) {
+      response.status(500).json({ error: err.message });
+    }
+  });
+
+module.exports = studentsController;
